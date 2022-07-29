e19a1850170408e480ae60add7da29d7fbe80cfe
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-07-29 15:23:49 -0300

    basics, end of work on master branch

--- db/migrate/20220720191138_create_duties.rb
+++ db/migrate/20220720191138_create_duties.rb
@@ -1,6 +1,10 @@
 class CreateDuties < ActiveRecord::Migration[6.1]
   def change
     create_table :duties do |t|
+      t.string :date
+      t.boolean :adress
+      t.text :description
+      t.integer :payment
 
       t.timestamps
     end
--- db/migrate/20220720191157_create_works.rb
+++ db/migrate/20220720191157_create_works.rb
@@ -1,6 +1,8 @@
 class CreateWorks < ActiveRecord::Migration[6.1]
   def change
     create_table :works do |t|
+      t.references :work, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
